e9156602e1f8b9d95d1d0ea53d3d572f501b5d48
greales <g.realesguti@gmail.com>
2023-01-02 00:20:51 +0100

    add: Download Folder
    Added the functionality to download to another folder
    
    Also to download subtitles, but first tests were not succesful.

--- VidDown.py
+++ VidDown.py
@@ -12,16 +12,20 @@ def parse_args():
     parser.add_argument('--output', '-o', help='Output file name (default: <title>.mp4)')
     parser.add_argument('--audio', '-a', action='store_true', help='Download audio only', default=False)    
     parser.add_argument('--resolution', '-r', type=int, help='Maximum video resolution in pixels', default=360)
-    #parser.add_argument('--outputdir', '-o', help='Output directory', default='.')
+    parser.add_argument('--outdir', '-od', help='Output directory', default='Downloads')
+    parser.add_argument('--subtitles', '-s', action='store_true', help='Download subtitles', default=False)
     return parser.parse_args()
 
-def download_video(url, output, max_res):
+def download_video(url, output, max_res, subtitles):
     ydl_opts = {
         'format': f'bestvideo[height<={max_res}]+bestaudio/best[height<={max_res}]',        
         'merge_output_format': 'mp4',
         'outtmpl': output,
         'max_download_rate': 1000000,        
     }
+    if subtitles:
+        ydl_opts['writesubtitles'] = True
+        ydl_opts['allsubtitles'] = True
     with YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
@@ -51,16 +55,16 @@ if url:
             print('TITLE: '+title)
         if args.output is None:
             if args.audio:
-                output = f'{title}.mp3'
+                output = f'{args.outdir}/MP3/{title}.mp3'
             else:
-                output = f'{title}.mp4'
+                output = f'{args.outdir}/MP4/{title}.mp4'
         else:
             output = args.output
         # Download single video
         if args.audio:
                         download_audio(url, output)
         else:
-                        download_video(url, output, resolution)
+                        download_video(url, output, resolution, args.subtitles)
 elif url_list:
         # Read list of URLs from file and download all videos
         with open(url_list, 'r') as f:
@@ -73,13 +77,13 @@ elif url_list:
                     print('TITLE: '+title)
                 if args.output is None:
                     if args.audio:
-                        output = f'{title}.mp3'
+                        output = f'{args.outdir}/MP3/{title}.mp3'
                     else:
-                        output = f'{title}.mp4'
+                        output = f'{args.outdir}/MP4/{title}.mp4'
                 if args.audio:
                             download_audio(url, output)
                 else:
-                            download_video(url, output, resolution)
+                            download_video(url, output, resolution, args.subtitles)
 else:
         print('Error: No URL or URL list specified')
 
